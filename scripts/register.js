927438c69b5ca1716c60570fa7070ec2dd16fb6d
Scherazade Odoi <scherazadeodoi366@gmail.com>
2024-09-16 11:38:21 +0000

    registration login with javascript

---
+++ scripts/register.js
@@ -0,0 +1,11 @@
+// alert('JavaScript Works!');
+// select form by id
+const registerForm = document.getElementById('register-form');
+//  Handle form submit event
+registerForm.onsubmit = function() {
+// Collect Username
+// Collect email
+// Collect password
+// Save user information
+// Send confirmation email
+}
